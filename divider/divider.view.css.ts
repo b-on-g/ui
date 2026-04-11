163ebcd04101a5bc8c25381d4e8fd54a7ad0982b
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-04-11 21:55:03 +0300

    df

--- divider/divider.view.css.ts
+++ divider/divider.view.css.ts
@@ -1,8 +1,8 @@
 namespace $ {
 	$mol_style_define($bog_ui_divider, {
 		margin: {
-			bottom: $mol_gap.page,
-			top: $mol_gap.page,
+			bottom: $mol_gap.block,
+			top: $mol_gap.blocka,
 		},
 		width: '100%',
 		border: {
